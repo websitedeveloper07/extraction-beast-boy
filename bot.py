eab0d46e376cbee34cedaba94314cb464f51d2f4
shooters <aadityamoga2@gmail.com>
2025-11-30 13:03:54 +0530

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from html import unescape
 from datetime import datetime, timezone, timedelta
 
 # === CONFIG ===
-BOT_TOKEN = "8281535597:AAH8cE1IHd8gciFTLkqx_fizHsV0fuK6A1Q"
+BOT_TOKEN = "8588396684:AAGeVNBfC6dVK3G6Kuz8wQEeoVQYfSv0AIM"
 OWNER_IDS = {8516723793, 8278658138}  # Bot owners
 AUTHORIZED_USER_IDS = set(OWNER_IDS)
 PLAN = "PRO PLAN⚡"
